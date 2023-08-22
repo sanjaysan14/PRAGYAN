076a20198dfa0572306ac24067523e3f9e41e120
sanjay s <sanjaysan7788@gmail.com>
2023-08-22 14:25:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Corasal from './Corasal';
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Corasal/>
+    </>
+  );
+}
+
+export default App;
---
+++ Corasal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Corasal = () => {
+  return <div>Corasal</div>;
+};
+
+export default Corasal;
